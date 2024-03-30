d2c10c6e45828a4c0e048a6467c0b792eee75bc1
Filipe Meireles <filipe.ia.meireles@gmail.com>
2024-03-30 20:56:29 -0300

    iniciando

---
+++ out/_next/static/chunks/app/Equipe/page-5016200db8a28d41.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[942],{7070:function(e,n,t){Promise.resolve().then(t.t.bind(t,1749,23)),Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,9266,23))},9266:function(){}},function(e){e.O(0,[250,749,971,69,744],function(){return e(e.s=7070)}),_N_E=e.O()}]);
