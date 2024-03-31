f1936423c74396aabe8fad55aeacb9dcd4e7590d
Filipe Meireles <filipe.ia.meireles@gmail.com>
2024-03-30 21:10:21 -0300

    corrigindo basepath 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //basePath: "/testingGHPages",
+    basePath: "/testingGHPages",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
